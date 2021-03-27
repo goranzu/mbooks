164ea6a70a1d5671e178b62c809cd1a34c4c7d96
Goran <tenic.g@gmail.com>
2021-03-27 21:39:15 +0100

    showing results on search page

--- app/pages/_app.jsx
+++ app/pages/_app.jsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import PropTypes from "prop-types";
+import "../styles/globals.css";
+import { useRef } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClientRef = useRef();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any,
+};
+
+export default MyApp;
--- app/pages/search.jsx
+++ app/pages/search.jsx
@@ -1,34 +1,65 @@
 import useForm from "../lib/useForm";
 import axios from "axios";
+import { useMutation } from "react-query";
 
 export default function SearchPage() {
-  const { inputs, handleChange } = useForm({ title: "" });
+  const { inputs, handleChange } = useForm({ title: "enders game" });
+
+  const { mutate, status, data } = useMutation(() =>
+    axios.post("/api/search", inputs),
+  );
+
+  console.log(status);
+  console.log(data);
 
   return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        try {
-          const data = await axios.post("/api/search", inputs);
-          console.log(data.data);
-        } catch (error) {
-          console.error(error);
-        }
-      }}
-    >
-      <fieldset>
-        <label htmlFor="title">
-          Title
-          <input
-            value={inputs.title}
-            onChange={handleChange}
-            type="text"
-            name="title"
-            id="title"
-          />
-        </label>
-        <button type="submit">Search</button>
-      </fieldset>
-    </form>
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          try {
+            if (!inputs.title) return;
+            mutate();
+          } catch (error) {
+            console.error(error);
+          }
+        }}
+      >
+        <fieldset>
+          <label htmlFor="title">
+            Title
+            <input
+              value={inputs.title}
+              onChange={handleChange}
+              type="text"
+              name="title"
+              id="title"
+            />
+          </label>
+          <button type="submit">Search</button>
+        </fieldset>
+      </form>
+      {status === "loading" && <p>Loading...</p>}
+      {status === "error" && <p>Something went wrong please try again later</p>}
+      {status === "success" &&
+        data.data.data.books.map(
+          ({
+            title,
+            goodreadsId,
+            imageUrl,
+            publicationYear,
+            author,
+            averageRating,
+          }) => (
+            <article key={goodreadsId}>
+              <h2>{title}</h2>
+              <img src={imageUrl} alt={title} />
+              <p>Written by {author.name}</p>
+              <p>Published in {publicationYear}</p>
+              <p>rating: {averageRating}</p>
+            </article>
+          ),
+        )}
+    </>
   );
 }
