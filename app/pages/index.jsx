b4633aac0e7757b2ea474184d14949b5b38cdb6b
Goran <tenic.g@gmail.com>
2021-04-06 21:26:22 +0200

    homepage styling

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,23 +1,39 @@
 import Link from "next/link";
-import Page from "../components/Page";
+import styles from "../styles/homepage.module.css";
 
 export default function Home() {
   return (
-    <Page>
-      <h1>Hello World</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/search">Search</Link>
-          </li>
-          <li>
-            <Link href="/register">Register</Link>
-          </li>
-          <li>
-            <Link href="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
-    </Page>
+    <main className={styles.wrapper}>
+      <section className={styles.text}>
+        <h1>mBooks</h1>
+        <p>
+          Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
+          zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze
+          bedrijfstak sinds de 16e eeuw
+        </p>
+        <p>
+          toen een onbekende drukker een zethaak met letters nam en ze door
+          elkaar husselde om een font-catalogus te maken.
+        </p>
+        <div className={styles.buttons}>
+          <Link href="/register">
+            <a className="btn">Register</a>
+          </Link>
+          <Link href="/login">
+            <a className="btn btn--outline">Login</a>
+          </Link>
+        </div>
+      </section>
+      <div className={styles.imageWrapper}>
+        {/* <Image
+          src="/bag.png"
+          alt="bag"
+          layout="responsive"
+          width={395}
+          height={395}
+        /> */}
+        <img src="/bag.png" alt="bag" />
+      </div>
+    </main>
   );
 }
