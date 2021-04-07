2291a0fb171c86a334b9589aa4a5827060e496e5
Goran <tenic.g@gmail.com>
2021-04-07 22:04:47 +0200

    created modal for login and register

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,39 +1,59 @@
-import Link from "next/link";
+import { useState } from "react";
+import Modal from "../components/modal/Modal";
+import AuthForm from "../components/register-form/AuthForm";
 import styles from "../styles/homepage.module.css";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+  const [register, setRegister] = useState(false);
+
   return (
-    <main className={styles.wrapper}>
-      <section className={styles.text}>
-        <h1>mBooks</h1>
-        <p>
-          Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
-          zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze
-          bedrijfstak sinds de 16e eeuw
-        </p>
-        <p>
-          toen een onbekende drukker een zethaak met letters nam en ze door
-          elkaar husselde om een font-catalogus te maken.
-        </p>
-        <div className={styles.buttons}>
-          <Link href="/register">
-            <a className="btn">Register</a>
-          </Link>
-          <Link href="/login">
-            <a className="btn btn--outline">Login</a>
-          </Link>
+    <>
+      {showModal && (
+        <Modal>
+          <AuthForm
+            register={register}
+            handleModalClose={() => setShowModal(false)}
+          />
+        </Modal>
+      )}
+      <main className={styles.wrapper}>
+        <section className={styles.text}>
+          <h1>mBooks</h1>
+          <p>
+            Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
+            zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze
+            bedrijfstak sinds de 16e eeuw
+          </p>
+          <p>
+            toen een onbekende drukker een zethaak met letters nam en ze door
+            elkaar husselde om een font-catalogus te maken.
+          </p>
+          <div className={styles.buttons}>
+            <button
+              className="btn"
+              onClick={() => {
+                setRegister(true);
+                setShowModal(true);
+              }}
+            >
+              Register
+            </button>
+            <button
+              onClick={() => {
+                setRegister(false);
+                setShowModal(true);
+              }}
+              className="btn btn--outline"
+            >
+              Login
+            </button>
+          </div>
+        </section>
+        <div className={styles.imageWrapper}>
+          <img src="/bag.png" alt="bag" />
         </div>
-      </section>
-      <div className={styles.imageWrapper}>
-        {/* <Image
-          src="/bag.png"
-          alt="bag"
-          layout="responsive"
-          width={395}
-          height={395}
-        /> */}
-        <img src="/bag.png" alt="bag" />
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
